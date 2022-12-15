cd8b4f5d38e6a1554a9fa16f722a3c4eadea3fed
MateiMadalina <116349352+MateiMadalina@users.noreply.github.com>
2022-12-15 14:01:55 +0200

    'Final'

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ let gameTurn = 0;
 let currentPlayer;
 let board;
 
-
+//The empty function has a role in checking which available free positions we still have in the game
 function empty(board){
 let emptySpace = [];
 // for (let i = 0; i < board.length; i++) {
@@ -25,7 +25,7 @@ return emptySpace
 }
 
 
-
+//The function that knows that 'X will have moves on even positions and '0' on odd positions
 function orderPlayer(){
     if (gameTurn % 2 === 0) {
         currentPlayer = 'X';
@@ -45,6 +45,7 @@ function setGameMode(selectedValue) {
         case 'human-human':
             isPlayerXHuman = true;
             isPlayerYHuman = true;
+            //Visibility setting according to the need of each case
             setHTMLvisibilityForInputGameMode(false);
             setHTMLvisibilityForInputHumanCoordinates(true);
             setHTMLvisibilityForInputAiCoordinatesInput(false);
@@ -57,7 +58,7 @@ function setGameMode(selectedValue) {
              setHTMLvisibilityForInputHumanCoordinates(true);
              setHTMLvisibilityForButtonLabeledReset(true);
             break;
-        case 'ai-ai':
+        case 'ai-ai'://Added AI vs AI game option
             isPlayerXHuman = false;
             isPlayerYHuman = false;
             setHTMLvisibilityForInputGameMode(false);
@@ -68,13 +69,8 @@ function setGameMode(selectedValue) {
             break;
 
     }
-    resetBoard();
-
-    // setHTMLvisibilityForInputGameMode(false);
-    // setHTMLvisibilityForInputHumanCoordinates(true);
-    // setHTMLvisibilityForInputAiCoordinatesInput(false);
-    // setHTMLvisibilityForButtonLabeledReset(true);
-    displayMessage("Player X's turn");
+    resetBoard();//Resetting the board every time the game type is changed
+    displayMessage("Player X's turn");//the game always starts with X so this message will always be displayed at the beginning of the game
 }
 
 // this function is called whenever the user presses the `enter`
@@ -83,11 +79,13 @@ function setGameMode(selectedValue) {
 function processHumanCoordinate(input) {
     console.log(`'processHumanCoordinate('${input}')`);
     orderPlayer();
-
+//keep the coordinate on which the move was placed, eg: "A1"-> [0,0]
     let coordinates = extractCoordinates(input);
+ //If the typed position is empty, then moving the player will be possible, otherwise a warning message will be displayed in the game
     if (board[coordinates.x][coordinates.y] === ""){
         board[coordinates.x][coordinates.y] = currentPlayer;
         gameTurn += 1;
+//If the current player is AI then only the position generation button will be displayed
         if (isPlayerYHuman === false) {
             setHTMLvisibilityForInputHumanCoordinates(false);
             setHTMLvisibilityForInputAiCoordinatesInput(true);
@@ -95,7 +93,7 @@ function processHumanCoordinate(input) {
     } else{
         displayMessage("Position is already taken on board")
     }
-    
+//initializing a variable with the empty function in order to know when the table of x and 0 was completely occupied
     let go = empty(board);
 
     if (go.length === 0){
@@ -106,6 +104,7 @@ function processHumanCoordinate(input) {
         setHTMLvisibilityForButtonLabeledReset(true);
     }
  
+//initializing the variable with the 'getWinningPlayer' function in order to signal victory when we are in a winning situation
     const winningPlayer = getWinningPlayer(board);
     if (winningPlayer) {
         displayMessage(`Player ${currentPlayer} has won !`);
@@ -124,6 +123,7 @@ function processHumanCoordinate(input) {
 function processAICoordinate() {
 orderPlayer();
 
+//initializing a variable with the empty function in order to know when the table of x and 0 was completely occupied
 let go = empty(board);
 if (go.length === 0){
     displayMessage("It's a tie");
@@ -132,7 +132,10 @@ if (go.length === 0){
     setHTMLvisibilityForInputAiCoordinatesInput(false);
     setHTMLvisibilityForButtonLabeledReset(true);
 }
+
+//For the 'human-ai' case, in this function currentPlayer will always be '0', and to anticipate the movements of 'X', we created the variable secondPlayer
 let secondPlayer = 'X';
+//For the 'ai-ai' case, there would be a fluctuation of x and 0 for currentPlayer and secondPlayer
 if(currentPlayer === 'X'){
     secondPlayer = '0'
 }else{
@@ -140,26 +143,37 @@ if(currentPlayer === 'X'){
 }
 //console.log(secondPlayer);
 
+
+//The function so that 'ai' can see the situations in which he can lose and put the move in such a way as to prevent the loss
 let easyLose = getUnbeatableAiCoordinates(board, secondPlayer);
-console.log(easyLose);
+//console.log(easyLose);
+//if the function getUnbeatableAiCoordinates is used, then currentPlayer should take the position indicated by easyLose (variable initialized with the function)
 if(easyLose){
     board[easyLose[0]][easyLose[1]] = currentPlayer;
     gameTurn += 1;
     displayBoard(board);
-}else{
+}
+//If easyLose is not called, then 'ai' enters the loop of easyWin, in which certain moves are set so that he cannot be defeated
+else{
 let easyWin = getUnbeatableAiCoordinates(board, currentPlayer);
-console.log(easyWin)
+//console.log(easyWin)
+//on the if loop if 'ai' sees an opportunity to win, his move will be put in that position
 if(easyWin){
     board[easyWin[0]][easyWin[1]] = currentPlayer;
     gameTurn += 1;
     displayBoard(board);
-}else{
+}
+//But if he doesn't see a possible winning move, he checks several cases where he could win or tie
+else{
+//First of all, he checks if the position in the middle is empty, and if it is empty, this will be his move    
     if(board[1][1] === ''){
        board[1][1] = currentPlayer; 
        gameTurn += 1;
        displayBoard(board);
-    }else 
-    {
+    }
+    else{
+//Make a check if the middle is already taken by him, and if it is taken and the opponent has the corners of the table taken,
+//then he will generate the movement on a middle position (crosswise)
         if(board[1][1] === currentPlayer){
         if((board[0][0] === board[2][2] && board[0][0] !== '') || (board[0][2] === board[2][0] && board[0][2] !== '' )){
          let middle = [[0, 1], [1,0], [1,2], [2,1]];
@@ -175,6 +189,7 @@ if(easyWin){
                 displayBoard(board);
                 }
             }
+//If the opposite corners are not taken by the opponent, then it will generate a move to any available position
                 else{
                     let randomAiMove = go[Math.floor(Math.random() * go.length)]
                     board[randomAiMove[0]][randomAiMove[1]] = currentPlayer;
@@ -182,6 +197,7 @@ if(easyWin){
                     displayBoard(board);
                 }
         }
+//If the middle is not taken by him, then it will generate a random movement on any free corner
         else{
         let corner = [[0, 0], [0, 2], [2, 0], [2, 2]]
         let freeCorner = [];
@@ -197,6 +213,7 @@ if(easyWin){
         gameTurn += 1;
         displayBoard(board);
         }
+    //If the corners are exhausted, movement will be randomly generated on any other free position
         else{
             let randomAiMove = go[Math.floor(Math.random() * go.length)]
             board[randomAiMove[0]][randomAiMove[1]] = currentPlayer;
@@ -212,6 +229,7 @@ if(easyWin){
         setHTMLvisibilityForInputHumanCoordinates(true);
         setHTMLvisibilityForInputAiCoordinatesInput(false);
     }
+//Calling the function for signaling the victory and displaying it   
     const winningPlayer = getWinningPlayer(board);
     if (winningPlayer) {
         displayMessage(`Player ${currentPlayer} has won !`);
@@ -252,7 +270,7 @@ function extractCoordinates(input) {
         case "C3": return { x: 2, y: 2 };
     }
 let cases = ["A1", "A2", "A3", "B1", "B2", "B3", "C1", "C2", "C3"]
-
+//If a non-existing coordinate is entered in the game, a message will be displayed
     if (!cases.includes(input)){
        displayMessage("Invalid coordinate entered")  
 
@@ -262,6 +280,8 @@ let cases = ["A1", "A2", "A3", "B1", "B2", "B3", "C1", "C2", "C3"]
 
 // this function should return `X` or `O` or undefined (carefull it's not a string )
 // based on interpreting the values in the board variable
+
+//The cases by which the players can win 
 function getWinningPlayer(board) {
    if(board[0][0] === board[0][1] && board[0][0] === board[0][2] && board[0][0] !== '') {return board[0][0]}
    if(board[1][0] === board[1][1] && board[1][0] === board[1][2] && board[1][0] !== '') {return board[1][0]}
@@ -273,6 +293,7 @@ function getWinningPlayer(board) {
    if(board[0][2] === board[1][1] && board[0][2] === board[2][0] && board[0][2] !== '') {return board[0][2]}
 }
 
+//Cases in which players can prevent loss
 const getUnbeatableAiCoordinates = (board, player) => { 
 //1
 if(board[0][0] === board[0][1]  && board[0][2] === '' && board[0][0] === player) {return [0, 2] }
@@ -304,12 +325,12 @@ if(board[0][2] === board[1][2]  && board[2][2] === '' && board[0][2] === player)
 if(board[0][2] === board[2][2]  && board[1][2] === '' && board[2][2] === player) {return [1, 2] }
 if(board[1][2] === board[2][2]  && board[0][2] === '' && board[2][2] === player) {return [0, 2] } 
 
-//
+//7
 if(board[0][0] === board[1][1]  && board[2][2] === '' && board[0][0] === player) {return [2, 2] }
 if(board[0][0] === board[2][2]  && board[1][1] === '' && board[0][0] === player) {return [1, 1] }
 if(board[1][1] === board[2][2]  && board[0][0] === '' && board[2][2] === player) {return [0, 0] } 
 
-//
+//8
 if(board[0][2] === board[1][1]  && board[2][0] === '' && board[0][2] === player) {return [2, 0] }
 if(board[0][2] === board[2][0]  && board[1][1] === '' && board[2][0] === player) {return [1, 1] }
 if(board[1][1] === board[2][0]  && board[0][2] === '' && board[1][1] === player) {return [0, 2] } 
